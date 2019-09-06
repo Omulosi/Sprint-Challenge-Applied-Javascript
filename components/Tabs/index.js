1b0a3d07c0664cacc0852609d01a230f02cca62b
Omulosi <mulongojohnpaul@gmail.com>
2019-09-06 11:43:20 +0300

    Create Tab components and add them to DOM

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const API = "https://lambda-times-backend.herokuapp.com/topics";
+
+function Tab(topic) {
+  const tab = document.createElement('div');
+  tab.classList.add('tab');
+  tab.textContent = topic;
+
+  return tab;
+}
+
+const topics = document.querySelector('.topics');
+
+axios.get(API)
+  .then(response => response.data)
+  .then(data => {
+    data.topics.forEach(topic => {
+      topics.appendChild(Tab(topic));
+    })
+  })
